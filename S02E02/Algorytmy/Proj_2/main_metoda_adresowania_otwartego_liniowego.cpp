a9d7bad1ac369ae2ceaf6185fb8ce95cf1924fbf
SARUXSARU <lukasz.muras25@gmail.com>
2023-04-25 22:27:24 +0200

    hash_algorithms

---
+++ S02E02/Algorytmy/Proj_2/main_metoda_adresowania_otwartego_liniowego.cpp
@@ -0,0 +1,206 @@
+﻿
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+#include <ctime>
+#include<fstream>
+
+using namespace std;
+
+int counter = 0;
+
+int randX()
+{
+    int x = rand()%2147483647;
+    return x;
+}
+
+class dictionary {
+private:
+    int dictionary_size;
+public:
+    vector<int> dict;
+
+    dictionary(int ds=1000000)
+    {
+        this->dictionary_size = ds;
+        for (int i = 0; i < dictionary_size; i++)
+        {
+            dict.push_back(-1);
+        }
+    }
+    ~dictionary() = default;
+
+    int find(int i)
+    {
+        int hash_row = i % 10;
+    
+        
+        for (int j = hash_row; j < dict.size(); j++)
+        {
+            counter++;
+            if (dict[j] == -1)
+            {
+
+                return -1;
+            }
+            else if (dict[j] == i)
+            {
+             
+                return j;
+            }
+            else if (dict[j] == -2)
+            {
+                continue;
+            }
+        }
+
+        for (int j = 0; j < i % 10; j++)
+        {
+            counter++;
+            if (dict[j] == -1)
+            {
+             
+                return -1;
+            }
+            else if (dict[j] == i)
+            {
+               
+                return j;
+            }
+            else if (dict[j] == -2)
+            {
+                continue;
+            }
+        }
+        if (true)
+        {
+            return -1;
+        }
+
+
+    }
+
+    
+
+    void insert(int i)
+    {
+        int hash_row = i % 10;
+        bool inserted = false;
+        if (find(i)==-1)
+        {
+            for (int j = hash_row; j < dict.size(); j++)
+            {
+                if (dict[j] == -1)
+                {
+                    dict[j] = i;
+                    inserted = true;
+                    break;
+                }
+                else if (dict[j] == -2)
+                {
+                    dict[j] = i;
+                    inserted = true;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (inserted == false)
+            {
+                for (int j = 0; j < hash_row; j++)
+                {
+                    if (dict[j] == -1)
+                    {
+                        dict[j] = i;
+                        inserted = true;
+                        break;
+                    }
+                    else if (dict[j] == -2)
+                    {
+                        dict[j] = i;
+                        inserted = true;
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            
+        }
+        //if (inserted == false)
+        //{
+        //    //cout << "there is no space for this value" << endl;
+        //}
+
+    }
+
+    void delete_value(int i)
+    {
+
+        int index = find(i);
+        if (index != -1)
+        {
+            dict[index] = -2;
+        }
+    }
+
+    void print()
+    {
+        for (int i = 0; i < dict.size(); i++)
+            cout << dict[i] << ", ";
+    }
+
+
+
+};
+
+class test: public dictionary{
+
+    int numberOfValuesInseted_Founded;
+public:
+    
+    test(int val=10): numberOfValuesInseted_Founded(val)
+    {
+      
+    }
+    ~test() = default;
+    
+    void TestDataGeneric(test T)
+    {
+        ofstream save("C:/Users/user/Desktop/Algo proj/proj2_2/porownania.txt");
+        for (int i = 0; i < numberOfValuesInseted_Founded; i++)
+        {
+            int x = randX();
+          
+            T.insert(x);
+            counter = 0;
+            int y = randX();
+            T.find(y);
+          
+            save << counter << endl;
+            counter = 0;
+        }
+        save.close();
+
+        //T.print();
+    }
+
+};
+int main()
+{
+    srand(time(NULL));
+    test T(200000);
+    T.TestDataGeneric(T);
+    cout << "done";
+   
+    return 0;
+}
+
+
---
+++ S02E02/Algorytmy/Proj_2/main_metoda_lancuchowa.cpp
@@ -0,0 +1,134 @@
+﻿
+#include <iostream>
+#include<vector>
+#include<ctime>
+#include<stdlib.h>
+#include<fstream>
+
+using namespace std;
+
+int counter = 0;
+
+int randX()
+{
+    int x = rand() % 2147483647;
+    return x;
+}
+
+class dictionary {
+
+private:
+    int dictioary_size = 10;
+public:
+    vector<vector<int>> dict;
+    dictionary()
+    {
+        for (int i = 0; i < dictioary_size; i++)
+        {
+            vector<int> row;
+            dict.push_back(row);
+        }
+    }
+
+    int find(int i)
+    {
+        int hash_column = i % 10;
+        int check = 0;
+        for (int j = 0; j < dict[hash_column].size(); j++)
+        {
+            counter++;
+            if (dict[hash_column][j] == i)
+            {
+                return j;
+            }
+        }
+        return -1;
+        
+    }
+
+    void delete_value(int i)
+    {
+        int hash_row = i % 10;
+        int f = find(i);
+        if (f!=-1)
+        {
+            dict[hash_row].erase(dict[hash_row].begin() + f);
+        }
+        else
+        {
+            cout << "nothing to delete" << endl;
+        }
+    }
+
+   /* void print()
+    {
+        for (int i = 0; i < dictioary_size; i++)
+        {   
+            cout << i << ": ";
+            for (int j = 0; j < dict[i].size(); j++)
+            {
+                cout << dict[i][j] << ", ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }*/
+
+    void insert(int i)
+    {
+        int value = i;
+        int hash_column = value % 10;
+
+        while (find(value) != -1)
+        {
+            value = randX();
+            hash_column = value % 10;
+        }
+
+        dict[hash_column].push_back(value);
+    }
+
+};
+
+
+
+class test{
+
+public:
+    test() {}
+    ~test() = default;
+    dictionary D;
+
+    void insert_values_to_dictionary(int NumberOfElementsInDictionary)
+     {
+        ofstream save("C:/Users/user/Desktop/Algo proj/proj2_1/porownania.txt");
+         for (int i = 0; i < NumberOfElementsInDictionary; i++)
+         {
+             D.insert(randX());
+             counter = 0;
+             int x = randX();
+             D.find(x);
+             
+             save << counter << endl;
+             
+             counter = 0;
+         }
+         save.close();
+     }
+
+};
+    
+
+int main()
+{
+    srand(time(NULL));
+    test T;
+    T.insert_values_to_dictionary(30000);
+    //T.print();
+    cout << "done" << endl;
+
+
+    
+
+    return 0;
+}
